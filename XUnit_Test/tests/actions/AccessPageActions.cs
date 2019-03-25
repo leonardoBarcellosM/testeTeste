daaffebda4d0e545696a1cf02c38d10bac6fc55d
leonardoBarcellosM <leonardo.barcellos@viaflow.com.br>
2019-03-25 14:13:18 -0300

    Teste 014

--- XUnit_Test/tests/actions/AccessPageActions.cs
+++ XUnit_Test/tests/actions/AccessPageActions.cs
@@ -25,9 +25,9 @@ namespace XUnit_Test.tests.steps
             //options.AddArguments("start-maximized"); // open Browser in maximized mode
             //options.AddArguments("disable-infobars"); // disabling infobars
             //options.AddArguments("--disable-extensions"); // disabling extensions
+            options.AddArguments("--no-sandbox"); // Bypass OS security model
             options.AddArguments("--disable-gpu"); // applicable to windows os only
             options.AddArguments("--disable-dev-shm-usage"); // overcome limited resource problems
-            options.AddArguments("--no-sandbox"); // Bypass OS security model
             options.AddArguments("--headless");
             Driver = new ChromeDriver(path, options);
 
