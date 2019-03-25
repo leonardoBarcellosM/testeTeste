232699e76df202727980500674dec2af48c00a74
leonardoBarcellosM <leonardo.barcellos@viaflow.com.br>
2019-03-25 13:31:43 -0300

    Teste 011

--- XUnit_Test/tests/actions/AccessPageActions.cs
+++ XUnit_Test/tests/actions/AccessPageActions.cs
@@ -22,8 +22,14 @@ namespace XUnit_Test.tests.steps
             //string path = $"{currentPath}\\..\\..\\..\\WebDriverWindows";
 
             ChromeOptions options = new ChromeOptions();
-            options.AddArguments("--disable-dev-shm-usage");
+            options.AddArguments("start-maximized"); // open Browser in maximized mode
+            options.AddArguments("disable-infobars"); // disabling infobars
+            options.AddArguments("--disable-extensions"); // disabling extensions
+            options.AddArguments("--disable-gpu"); // applicable to windows os only
+            options.AddArguments("--disable-dev-shm-usage"); // overcome limited resource problems
+            options.AddArguments("--no-sandbox"); // Bypass OS security model
             Driver = new ChromeDriver(path, options);
+            
 
             //if (Environment.OSVersion.Platform == PlatformID.Win32NT)
             //{
@@ -68,7 +74,6 @@ namespace XUnit_Test.tests.steps
             }
             return _result;
 
-            Driver.Quit();
         }
 
         public bool ValidarAcessoIes()
